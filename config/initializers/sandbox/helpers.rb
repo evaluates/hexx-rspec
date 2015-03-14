ff691d7128338f98dc9af20cb3f3d427c08d3c85
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-14 20:39:14 +0300

    [FEATURE] RSpec tag :sandbox
    When :sandbox is used:
    
    * Sandbox is created at 'tmp/sandbox' before each test
    * #try_in_sandbox helper method defined
    * #read_in_sandbox helper method defined
    * be_present_in_sandbox matcher defined
    * be_absent_in_sandbox matcher defined

---
+++ config/initializers/sandbox/helpers.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+require "yaml"
+
+# Returns the path to the temporary `spec/sandbox`.
+# @return [String] The absolute path.
+def sandbox
+  @sandbox ||= File.expand_path "../tmp/sandbox"
+end
+
+# Clears the temporary `spec/sandbox`.
+def clear_sandbox
+  FileUtils.rm_rf sandbox
+end
+
+# Re-creates the temporary `spec/sandbox`.
+def prepare_sandbox
+  clear_sandbox
+  FileUtils.mkdir_p sandbox
+end
+
+# Runs code from the temporary `spec/sandbox`.
+def try_in_sandbox
+  FileUtils.cd(sandbox) { yield }
+end
+
+# Reads file in sandbox and returns file content.
+# Returns a blank string when the file is absent.
+# @return [String] The content.
+def read_in_sandbox(filename)
+  file = Dir[File.join(sandbox, filename)].first
+  file ? File.read(file) : ""
+end
